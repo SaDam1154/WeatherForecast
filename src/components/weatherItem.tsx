56e82d80488832de67f0b848b78526e4ecaa373e
Sa Đam <20521154@gm.uit.edu.vn>
2024-10-25 15:17:59 +0700

    update interface

--- src/components/weatherItem.tsx
+++ src/components/weatherItem.tsx
@@ -1,7 +1,7 @@
-import { Weather } from '../types/weather';
-interface weatherItemProps {
-    weather: Weather;
-}
-export default function WeatherItem({ weather }: weatherItemProps) {
-    return <div>{weather.title}</div>;
-}
+// import { Weather } from '../types/weather';
+// interface weatherItemProps {
+//     weather: Weather;
+// }
+// export default function WeatherItem({ weather }: weatherItemProps) {
+//     return <div>{weather.title}</div>;
+// }
