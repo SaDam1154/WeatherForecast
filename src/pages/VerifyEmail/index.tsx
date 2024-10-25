33e0e1cf99cd281c3380d01f01c721d78785709e
Sa Đam <20521154@gm.uit.edu.vn>
2024-10-26 00:13:57 +0700

    update Readme

--- src/pages/VerifyEmail/index.tsx
+++ src/pages/VerifyEmail/index.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-const VerifyEmail: React.FC = () => {
+const VerifyEmail = () => {
     const [isVerified, setIsVerified] = useState(false);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState('');
